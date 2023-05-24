b6dedbe0ed84775058e56b510ce68753367d7e1a
Jleagle <jimeagle@gmail.com>
2023-05-24 13:40:18 +0100

    Check for bucket

--- rate.go
+++ rate.go
@@ -125,5 +125,8 @@ func SetRateLimitHeaders(w http.ResponseWriter, limiters *Limiters, reservation 
 	w.Header().Set("X-RateLimit-Every", limiters.GetMinInterval().String())
 	w.Header().Set("X-RateLimit-Burst", fmt.Sprint(limiters.GetBurst()))
 	w.Header().Set("X-RateLimit-Wait", reservation.Delay().String())
-	w.Header().Set("X-RateLimit-Bucket", limiters.GetBucketName())
+
+	if bucket := limiters.GetBucketName(); bucket != "" {
+		w.Header().Set("X-RateLimit-Bucket", bucket)
+	}
 }
